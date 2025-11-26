a3a77134e47ac6e4fec12c118e676440f3196f16
kefcabz <kefbball@gmail.com>
2025-11-26 16:19:59 -0500

    Prison Movement Script

---
+++ Assets/Scripts/PrisonerMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PrisonerMovement : MonoBehaviour
+{
+    public Vector3 targetPosition;
+    public float speed = 6f;
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            targetPosition,
+            speed * Time.deltaTime
+        );
+
+        Vector3 dir = targetPosition - transform.position;
+        if (dir.sqrMagnitude > 0.01f)
+        {
+            transform.rotation = Quaternion.LookRotation(dir);
+        }
+    }
+}
